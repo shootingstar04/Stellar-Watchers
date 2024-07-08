19a45eaba00d35c0c4f190d47ef169898a227c58
20241103 <162987094+waariioo@users.noreply.github.com>
2024-07-08 15:24:27 +0900

    시점이동 수정완

--- Assets/Scripts/cam_deadzone_test.cs
+++ Assets/Scripts/cam_deadzone_test.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Cinemachine;
+using UnityEngine;
 
 public class cam_deadzone_test : MonoBehaviour
 {
@@ -9,46 +7,85 @@ public class cam_deadzone_test : MonoBehaviour
     private CinemachineFramingTransposer camT;
 
     float lastMinput;
+    bool isAttacking;
+    private Vector3 priviousPos;
+    private float vel;
 
+    float moveInput;
+    float sightInput;
+    bool jumpInput;
+    bool lookAround;
+
+    float timer = 0f;
 
     private void Start()
     {
         cam = GetComponent<CinemachineVirtualCamera>();
         camT = cam.GetCinemachineComponent<CinemachineFramingTransposer>();
+        priviousPos = this.transform.position;
+
+
+
     }
 
     private void Update()
     {
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        float sightInput = Input.GetAxisRaw("Vertical");
 
-        bool jumpInput = Input.GetKey(KeyCode.Z);
+        moveInput = Input.GetAxisRaw("Horizontal");
+        sightInput = Input.GetAxisRaw("Vertical");
+
+        jumpInput = Input.GetKey(KeyCode.Z);
+        isAttacking = Input.GetKey(KeyCode.X);
+
+
+        if (sightInput != 0f)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 1f)
+            {
+                    timer = 0f;
+                    lookAround = true;
+            }
+        }
+
+        if(sightInput== 0f || moveInput != 0 || isAttacking || jumpInput)
+        {
+            timer = 0f;
+            lookAround = false;
+        }
+
+        Vector3 currentPos = transform.position;
+        vel = (currentPos - priviousPos).magnitude / Time.deltaTime;
+        priviousPos = currentPos;
 
         if (moveInput != 0)
             lastMinput = moveInput;
 
         camT.m_ScreenX = 0.5f - (lastMinput * 0.05f);
-        /*
-        if (moveInput == 1f)
-            camT.m_ScreenX = 0.45f;
-        else if (moveInput == -1f)
-            camT.m_ScreenX = 0.55f;
-        else if (moveInput == 0f)
-            camT.m_ScreenX = 0.5f - lastMinput * 0.05f;
-        */
 
         //논리구조 오류 조심 -- 점프와 위아래 화살표 사이 안겹치게
-        if (jumpInput && sightInput == 0)
+        //공격, 이동중 시야이동 안되게...
+        if (jumpInput)
             camT.m_ScreenY = 0.65f;
-        else if (!jumpInput && sightInput == 0)
+
+        else if (!jumpInput)
+        {
             camT.m_ScreenY = 0.55f;
-        else if (sightInput == 1f)
-            camT.m_ScreenY = 1.0f;
-        else if (sightInput == -1f)
+
+            if (moveInput == 0)
+                cameracontol();
+        }
+    }
+
+    private void cameracontol()
+    {
+        if (!isAttacking && lookAround && sightInput == 1f)
+            camT.m_ScreenY = 0.8f;
+
+        else if (!isAttacking && lookAround && sightInput == -1f)
             camT.m_ScreenY = 0.3f;
-        else
+
+        else if (lookAround && sightInput == 0f)
             camT.m_ScreenY = 0.5f;
-
-
     }
 }
