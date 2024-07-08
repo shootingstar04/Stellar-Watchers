95d3d755d5369bfaee107913c808da51dca6ddda
20241103 <162987094+waariioo@users.noreply.github.com>
2024-07-08 17:47:18 +0900

    ㅁㅁㄴㅇ;ㅏ므ㅓ리ㅏ무디

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    private Vector2 parallaxEffectMultiplier = new Vector2(0.4f, 0.1f);
+    [SerializeField] private Vector2 parallaxEffectMultiplier;
 
     private Transform CameraTransform;
     private Vector3 lastCameraPosition;
