d067b753df6bced33426542106e0e2bfe752b328
20241103 <162987094+waariioo@users.noreply.github.com>
2024-07-17 11:38:12 +0900

    맵 이동 기능구현

--- Assets/Scripts/MapTeleporter.cs
+++ Assets/Scripts/MapTeleporter.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class MapTeleporter : MonoBehaviour
 {
+    [SerializeField] int offset;
+    [SerializeField] Transform TargetPosition;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag(Define.PlayerTag))
         {
-            SceneManager.instance.NextMap();
+            Debug.Log("맵전환 작동");
+            SceneManager.instance.NextMap(TargetPosition, offset);
         }
     }
 }
