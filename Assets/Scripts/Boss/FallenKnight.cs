b7b41583ab0df353f271b5bce8ec856005120996
Shooting Star <76259618+shootingstar04@users.noreply.github.com>
2024-07-23 17:27:46 +0900

    헤비 아머2 Ai 작업

--- Assets/Scripts/Boss/FallenKnight.cs
+++ Assets/Scripts/Boss/FallenKnight.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeavyArmor2 : MonoBehaviour
+public class FallenKnight : MonoBehaviour
 {
     public enum State
     {
