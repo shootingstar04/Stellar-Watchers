1b0e2054f76bbe9400944f2f48f3bd06eebe67a5
1405 <163080064+hdo1405@users.noreply.github.com>
2024-07-29 14:52:16 +0900

    비밀벽 완성

--- Assets/Scripts/Object/secret_map/secret_wall.cs
+++ Assets/Scripts/Object/secret_map/secret_wall.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Tilemaps;
 
 public class secret_wall : MonoBehaviour
-{
+{ 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,26 @@ public class secret_wall : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(Define.PlayerTag))
+        {
+            Color color = Color.white;
+
+            color.a = 0.5f;
+            this.GetComponent<Tilemap>().color = color;
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(Define.PlayerTag))
+        {
+            Color color = Color.white;
+            this.GetComponent<Tilemap>().color = color;
+        }
     }
 }
